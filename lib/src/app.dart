707e3b561133af76e4cd41b0b568050656edbbe5
Sandithya21 <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2024-09-05 18:35:42 +0530

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,12 +8,8 @@ import 'package:trashtrek/src/user_management_feature/signin.dart';
 import 'package:trashtrek/src/user_management_feature/signup.dart';
 import 'package:trashtrek/src/user_management_feature/userProfile.dart';
 
-import 'reward_management/screens/add_rating_view.dart';
-import 'reward_management/screens/admin_driver_profile.dart';
-import 'reward_management/screens/view_reviews.dart';
 import 'reward_management/add_rating_view.dart';
 import 'reward_management/admin_driver_profile.dart';
-import 'reward_management/driver_profile.dart';
 import 'reward_management/view_reviews.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
