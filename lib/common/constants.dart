33eb7bddfb574bcfca4d3ab614cd16f358650e9e
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2024-09-05 10:00:16 +0530

    constatnt update

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,10 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Constants {
+  // .env
+  static const baseURL = 'BASE_URL';
+
+  // Route Names
   static const wasteMapRoute = '/';
   static const appointmentsRoute = '/appointments';
+  static const myAppointmentsRoute = '/my_appointments';
 }
