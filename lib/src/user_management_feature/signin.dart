841e38be573d502f60e7bf8e9bccc02fcbf74f8e
Osadha Ganewaththa <it21802058@my.sliit.lk>
2024-09-09 04:40:11 +0530

    user profile edit added

--- lib/src/user_management_feature/signin.dart
+++ lib/src/user_management_feature/signin.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trashtrek/common/constants.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:trashtrek/src/sample_feature/sample_item_list_view.dart';
+//import 'package:trashtrek/src/sample_feature/sample_item_list_view.dart';
 
 class SignIn extends StatefulWidget {
   @override
