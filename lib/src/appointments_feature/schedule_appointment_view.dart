564161cfc4b3dce2e338b5327ed3511f93a02402
AvishkaNuwan <it21256264@my.sliit.lk>
2024-09-10 20:17:04 +0530

    add google map front

--- lib/src/appointments_feature/schedule_appointment_view.dart
+++ lib/src/appointments_feature/schedule_appointment_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:trashtrek/common/constants.dart';
 import 'package:trashtrek/src/appointments_feature/appointment_model.dart';
 import 'package:trashtrek/src/appointments_feature/schedule_appointment_service.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class ScheduleAppointmentView extends StatefulWidget {
   final ApiService apiService;
@@ -14,14 +16,26 @@ class ScheduleAppointmentView extends StatefulWidget {
       _ScheduleAppointmentViewState();
 }
 
-class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
+class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView>
+    with WidgetsBindingObserver {
   final _formKey = GlobalKey<FormState>();
   final _dateController = TextEditingController();
   final _houseNoController = TextEditingController();
   final _streetController = TextEditingController();
   final _cityController = TextEditingController();
   String _status = 'pending';
-  bool _isLoading = false; // Loading state variable
+  bool _isLoading = false;
+  LatLng _selectedLocation = LatLng(0, 0);
+
+  late GoogleMapController _mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkPermissions();
+    });
+  }
 
   @override
   void dispose() {
@@ -30,6 +44,98 @@ class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
     _streetController.dispose();
     _cityController.dispose();
     super.dispose();
+  }
+
+  Future<void> _checkPermissions() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Location permissions are denied.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+        Navigator.pushNamed(context, '/options');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied. Please enable them in settings.'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      Navigator.pushNamed(context, '/options');
+      return;
+    }
+
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable them in settings.'),
+          backgroundColor: Colors.red,
+          action: SnackBarAction(
+            label: 'Settings',
+            onPressed: () {
+              Geolocator.openLocationSettings();
+            },
+          ),
+        ),
+      );
+      Navigator.pushNamed(context, '/options');
+      return;
+    }
+
+    await _getUserLocation();
+  }
+
+  Future<void> _getUserLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Notify user and prompt them to enable location services
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable them in settings.'),
+          backgroundColor: Colors.red,
+          action: SnackBarAction(
+            label: 'Settings',
+            onPressed: () {
+              Geolocator.openLocationSettings();
+            },
+          ),
+        ),
+      );
+      return;
+    }
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      setState(() {
+        _selectedLocation = LatLng(position.latitude, position.longitude);
+        _mapController.animateCamera(CameraUpdate.newLatLng(_selectedLocation));
+      });
+    } catch (e) {
+      // Handle other exceptions such as permission denied
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to get location: $e'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 
   Future<void> _selectDate() async {
@@ -53,7 +159,7 @@ class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
   Future<void> _submitAppointment() async {
     if (_formKey.currentState?.validate() ?? false) {
       setState(() {
-        _isLoading = true; // Set loading state to true
+        _isLoading = true;
       });
 
       final userId = await widget.apiService.getUserId();
@@ -69,7 +175,6 @@ class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
       );
 
       try {
-// Check if the user has already scheduled an appointment for the selected date
         final hasAppointment =
             await widget.apiService.hasAppointment(appointment.date);
 
@@ -106,7 +211,7 @@ class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
         );
       } finally {
         setState(() {
-          _isLoading = false; // Set loading state to false
+          _isLoading = false;
         });
       }
     }
@@ -154,65 +259,35 @@ class _ScheduleAppointmentViewState extends State<ScheduleAppointmentView> {
                     },
                   ),
                   const SizedBox(height: 16),
-                  TextFormField(
-                    controller: _houseNoController,
-                    decoration: const InputDecoration(
-                      labelText: 'House No',
-                      border: OutlineInputBorder(),
+                  SizedBox(
+                    height: 300,
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: _selectedLocation,
+                        zoom: 14.0,
+                      ),
+                      onMapCreated: (GoogleMapController controller) {
+                        _mapController = controller;
+                        _getUserLocation();
+                      },
+                      markers: {
+                        Marker(
+                          markerId: MarkerId('userLocation'),
+                          position: _selectedLocation,
+                        ),
+                      },
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the house number';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: _streetController,
-                    decoration: const InputDecoration(
-                      labelText: 'Street',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the street';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: _cityController,
-                    decoration: const InputDecoration(
-                      labelText: 'City',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the city';
-                      }
-                      return null;
-                    },
                   ),
                   const SizedBox(height: 24),
                   ElevatedButton(
                     onPressed: _isLoading ? null : _submitAppointment,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color.fromARGB(255, 94, 189, 149),
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ), // Disable button if loading
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
                     child: _isLoading
-                        ? const CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              Colors.white,
-                            ),
-                          )
-                        : const Text('Schedule Appointment'),
+                        ? const CircularProgressIndicator()
+                        : const Text('Submit Appointment'),
                   ),
                 ],
               ),
