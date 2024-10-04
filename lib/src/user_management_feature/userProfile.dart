a63c9b9656595351bf517c7b2ce422fdb63c3921
Osadha Ganewaththa <it21802058@my.sliit.lk>
2024-10-05 03:27:35 +0530

    report generation updted

--- lib/src/user_management_feature/userProfile.dart
+++ lib/src/user_management_feature/userProfile.dart
@@ -22,7 +22,7 @@ class _UserProfileState extends State<UserProfile> {
   String? _phoneNo;
   String? _userType;
   String? _address;
-  bool _isLoading = false; // Add this variable to track loading state
+  bool _isLoading = true; // Set to true initially
 
   Future<void> _loadUserData() async {
     final prefs = await SharedPreferences.getInstance();
@@ -48,24 +48,27 @@ class _UserProfileState extends State<UserProfile> {
         _userType = data['userType'];
         _address =
             "${data['address']['houseNo']}, ${data['address']['street']}, ${data['address']['city']}";
+        _isLoading = false; // Stop loading after data is fetched
       });
     } else {
       // Handle error
       print('Failed to load user data');
+      setState(() {
+        _isLoading = false; // Stop loading on error
+      });
     }
   }
 
   Future<void> _logout() async {
     setState(() {
-      _isLoading = true; // Start loading
+      _isLoading = true; // Start loading when logging out
     });
 
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
     final email = _email;
 
-    final baseUrl =
-        dotenv.env[Constants.baseURL]; // Get the base URL from the .env file
+    final baseUrl = dotenv.env[Constants.baseURL]; // Get the base URL
 
     final response = await http.post(
       Uri.parse('$baseUrl/users/logout'),
@@ -108,7 +111,6 @@ class _UserProfileState extends State<UserProfile> {
     } else {
       // Handle error
       print('Failed to logout');
-      // Optionally show an error dialog
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -139,104 +141,94 @@ class _UserProfileState extends State<UserProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar.appBar('User Profile'),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 133, 224, 125),
-              Color.fromARGB(255, 187, 251, 201),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Profile Icon (centered)
-                  const CircleAvatar(
-                    radius: 50,
-                    backgroundColor: Colors.grey,
-                    child: Icon(Icons.person, size: 50, color: Colors.white),
-                  ),
-                  const SizedBox(height: 10),
-                  // Edit Button (under the profile icon)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/editProfile');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          minimumSize: const Size(120, 30), // Smaller size
-                        ),
-                        child: const Text(
-                          "Edit Profile",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.settings),
-                        onPressed: () {
-                          // Navigate to the settings page.
-                          Navigator.restorablePushNamed(
-                              context, SettingsView.routeName);
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  // White box for user details and Remove Account Button
-                  Container(
-                    width: double.infinity,
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator()) // Show loading indicator
+          : Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 133, 224, 125),
+                    Color.fromARGB(255, 187, 251, 201),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        _buildUserInfo("Name", "$_firstName $_lastName"),
+                        const CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.grey,
+                          child:
+                              Icon(Icons.person, size: 50, color: Colors.white),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/editProfile');
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.settings),
+                              onPressed: () {
+                                Navigator.restorablePushNamed(
+                                    context, SettingsView.routeName);
+                              },
+                            ),
+                          ],
+                        ),
                         const SizedBox(height: 20),
-                        _buildUserInfo("Email", _email),
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              _buildUserInfo("Name", "$_firstName $_lastName"),
+                              const SizedBox(height: 20),
+                              _buildUserInfo("Email", _email),
+                              const SizedBox(height: 20),
+                              _buildUserInfo("Phone Number", _phoneNo),
+                              const SizedBox(height: 20),
+                              _buildUserInfo("Address", _address),
+                              const SizedBox(height: 20),
+                              _buildUserInfo("User Type", _userType),
+                              const SizedBox(height: 30),
+                            ],
+                          ),
+                        ),
                         const SizedBox(height: 20),
-                        _buildUserInfo("Phone Number", _phoneNo),
-                        const SizedBox(height: 20),
-                        _buildUserInfo("Address", _address),
-                        const SizedBox(height: 20),
-                        _buildUserInfo("User Type", _userType),
-                        const SizedBox(height: 30),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  // Logout Button
-                  _isLoading // Show progress indicator if loading
-                      ? const CircularProgressIndicator()
-                      : ElevatedButton(
+                        ElevatedButton(
                           onPressed: _logout,
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFF273F71),
-                            minimumSize: const Size(double.infinity, 50),
+                            minimumSize: const Size(150, 40),
                           ),
                           child: const Text(
                             "LOGOUT",
                             style: TextStyle(color: Colors.white),
                           ),
                         ),
-                ],
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
